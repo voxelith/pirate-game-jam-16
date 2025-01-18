2de37ca779b65e39c5ffbd49919037eb9e8e7ba5
voxelith <voxelith@protonmail.com>
2025-01-18 14:58:57 -0500

    Fixed some camera jitter

--- scripts/camera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera3D
-
-@onready var target := $".."
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	global_position = round(target.global_position * Globals.pixels_per_meter) / Globals.pixels_per_meter
