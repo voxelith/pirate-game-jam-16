74ac1ea6fbcc2ea5828d0f07c009231133ebff08
Loren Burkholder <loren@lorendb.dev>
2025-01-23 21:10:35 -0500

    Add pressure plate
    This is intended to be used primarily through Godot signals.

---
+++ assets/interactables/pressure_plate.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+# for querying from script
+@export var is_triggered: bool = false
+
+# for hooking up other elements
+signal triggered
+signal untriggered
+
+@onready var player = get_tree().current_scene.get_node("%Player")
+
+func _ready() -> void:
+	$Area3D.body_entered.connect(func(body):
+		if body == player:
+			is_triggered = true
+			triggered.emit()
+	)
+	$Area3D.body_exited.connect(func(body):
+		if body == player:
+			is_triggered = false
+			untriggered.emit()
+	)
