6043b7d17ef59b7c0ece219d2e51d6e78807bf68
voxelith <voxelith@protonmail.com>
2025-01-27 21:01:49 -0500

    Added animation/sfx to detonate screen

--- scripts/detonate_query_screen.gd
+++ scripts/detonate_query_screen.gd
@@ -5,9 +5,12 @@ extends Control
 signal accepted
 signal rejected
 
+func trigger_accepted():
+	accepted.emit()
+
 func _ready() -> void:
 	$HBoxContainer/DeathCount.text = "Death count: %d" % death_count
 	
 	$HBoxContainer/HBoxContainer/Yes.grab_focus()
-	$HBoxContainer/HBoxContainer/Yes.pressed.connect(func(): accepted.emit())
+	$HBoxContainer/HBoxContainer/Yes.pressed.connect(func(): $AnimationPlayer.play("detonate"))
 	$HBoxContainer/HBoxContainer/No.pressed.connect(func(): rejected.emit())
