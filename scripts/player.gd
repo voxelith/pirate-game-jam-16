e0889fbef51523ee652c35817ea31aa0cbafe822
voxelith <voxelith@protonmail.com>
2025-01-27 19:44:35 -0500

    Removed snapping, updated sensitivity for camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,18 +4,17 @@ const SPEED := 5.0
 const JUMP_VELOCITY := 3
 const FRICTION := 0.01
 
-const LOOK_SPEED := TAU / 11.25
+const LOOK_SPEED := TAU / 20.0
 const LOOK_FRICTION := 0.008
 const MOUSE_SENSIVITY := 0.5
 const MAX_MOUSE_MOVE := 100.0
 const MOUSE_COOLDOWN := 3.0
 
 const OCTAU = TAU / 8.0
-var look_spring := SpringFloat.new(1.0, 2.0, 0.0)
+var look_spring := SpringFloat.new(4.0, 4.0, 0.0)
 
 var look_rotation := 0.0
 var mouse_dir := 0.0
-var mouse_move_last := 0.0
 
 @onready var reticle = $Reticle
 @onready var anim = $AnimationPlayer
@@ -29,7 +28,6 @@ func reset_to_default() -> void:
 	destroyed_count = 0
 	look_rotation = 0
 	mouse_dir = 0
-	mouse_move_last = 0
 	# TODO: figure out how to reset the camera
 
 func _ready() -> void:
@@ -39,7 +37,6 @@ func _ready() -> void:
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
 		mouse_dir = clamp(-event.relative.x * MOUSE_SENSIVITY, -MAX_MOUSE_MOVE, MAX_MOUSE_MOVE)
-		mouse_move_last = 0.0
 
 func _physics_process(delta: float) -> void:
 	#################
@@ -71,12 +68,7 @@ func _physics_process(delta: float) -> void:
 	if mouse_dir:
 		look_dir = mouse_dir
 		mouse_dir = 0.0
-	mouse_move_last += delta
-	
-	# If it has been long enough since a mouse cooldown, snap camera angle to 45 deg. increment
-	if mouse_move_last > MOUSE_COOLDOWN:
-		look_spring.target = round(look_rotation / OCTAU) * OCTAU
-	
+
 	if look_dir:
 		look_spring.target = look_rotation + (look_dir * LOOK_SPEED)
 	
